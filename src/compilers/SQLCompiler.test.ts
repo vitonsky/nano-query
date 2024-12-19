c27725079d73d8b4b004c244ef168858887628a9
Robert Vitonsky <rob@vitonsky.net>
2024-12-20 00:36:35 +0100

    feat: support positional placeholders

--- src/compilers/SQLCompiler.test.ts
+++ src/compilers/SQLCompiler.test.ts
@@ -68,3 +68,47 @@ test('Compiler can process nested queries', () => {
 		bindings: [1, 'foo', 'bar', 2, 3],
 	});
 });
+
+describe('Compiler options', () => {
+	test('Compilation with positional placeholders', () => {
+		const compiler = new SQLCompiler({
+			getPlaceholder(index) {
+				return '$' + (index + 1);
+			},
+		});
+
+		expect(
+			compiler.compile(
+				new Query(
+					new RawSegment('SELECT *'),
+					new RawSegment(' '),
+					new RawSegment('FROM foo'),
+					new RawSegment(' '),
+					new RawSegment('WHERE x='),
+					new PreparedValue(1),
+					new RawSegment(' AND '),
+					new Query(
+						new RawSegment('('),
+						new RawSegment('SELECT y FROM bar WHERE n='),
+						new PreparedValue('foo'),
+						new RawSegment(' OR '),
+						new Query(new RawSegment('n2='), new PreparedValue('bar')),
+						new RawSegment(')'),
+					),
+					new RawSegment(' '),
+					new RawSegment('LIMIT'),
+					new RawSegment(' '),
+					new PreparedValue(2),
+					new RawSegment(' '),
+					new RawSegment('OFFSET'),
+					new RawSegment(' '),
+					new PreparedValue(3),
+				),
+			),
+		).toEqual({
+			command:
+				'SELECT * FROM foo WHERE x=$1 AND (SELECT y FROM bar WHERE n=$2 OR n2=$3) LIMIT $4 OFFSET $5',
+			bindings: [1, 'foo', 'bar', 2, 3],
+		});
+	});
+});
--- src/compilers/SQLCompiler.ts
+++ src/compilers/SQLCompiler.ts
@@ -11,30 +11,52 @@ export interface Compiler<T> {
 	compile: (query: Query) => T;
 }
 
+export type SQLCompilerConfig = {
+	getPlaceholder: (valueIndex: number) => string;
+};
+
 export class SQLCompiler implements Compiler<CommandWithBindings<QueryBindings>> {
+	private readonly config: SQLCompilerConfig;
+	constructor(options?: Partial<SQLCompilerConfig>) {
+		this.config = {
+			getPlaceholder: options?.getPlaceholder ?? (() => '?'),
+		};
+	}
 	/**
 	 * Compile query to SQL string and bindings
 	 */
 	public compile(query: Query) {
-		let command = '';
-		const bindings: Array<string | number | null> = [];
-		for (const segment of query.getSegments()) {
-			if (segment instanceof Query) {
-				const data = this.compile(segment);
-				command += data.command;
-				bindings.push(...data.bindings);
-				continue;
+		const sharedState = {
+			valueIndex: 0,
+		};
+		const processQuery = (query: Query): CommandWithBindings<QueryBindings> => {
+			let command = '';
+			const bindings: Array<string | number | null> = [];
+			for (const segment of query.getSegments()) {
+				if (segment instanceof Query) {
+					const data = processQuery(segment);
+					command += data.command;
+					bindings.push(...data.bindings);
+					continue;
+				}
+
+				if (segment instanceof PreparedValue) {
+					const placeholder = this.config.getPlaceholder(
+						sharedState.valueIndex,
+					);
+					sharedState.valueIndex++;
+
+					command += placeholder;
+					bindings.push(segment.getValue());
+					continue;
+				}
+
+				command += segment.getValue();
 			}
 
-			if (segment instanceof PreparedValue) {
-				command += '?';
-				bindings.push(segment.getValue());
-				continue;
-			}
+			return { command, bindings };
+		};
 
-			command += segment.getValue();
-		}
-
-		return { command, bindings };
+		return processQuery(query);
 	}
 }
